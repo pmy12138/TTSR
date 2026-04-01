1e49c529af1f7e4cca8835ecdc62d5b3a28a2317
pmy12138 <150931087+pmy12138@users.noreply.github.com>
2026-04-01 09:52:51 +0800

    Modify training parameters in train.sh
    Updated training parameters for the TTSR model, including dataset, batch size, and logging frequency.

--- train.sh
+++ train.sh
@@ -1,11 +1,13 @@
 ### training TTSR
-python main.py --save_dir ./train/CUFED/TTSR \
+#rmdir /s /q .\train\CUSTOM\TTSR
+/d/anaconda3/envs/pmypytorch2/python.exe main.py \
+               --save_dir ./train/CUSTOM/TTSR \
                --reset True \
                --log_file_name train.log \
                --num_gpu 1 \
-               --num_workers 9 \
-               --dataset CUFED \
-               --dataset_dir /home/v-fuyang/Data/CUFED/ \
+               --num_workers 4 \
+               --dataset CUSTOM \
+               --dataset_dir ./CUSTOM \
                --n_feats 64 \
                --lr_rate 1e-4 \
                --lr_rate_dis 1e-4 \
@@ -14,12 +16,40 @@ python main.py --save_dir ./train/CUFED/TTSR \
                --per_w 1e-2 \
                --tpl_w 1e-2 \
                --adv_w 1e-3 \
-               --batch_size 9 \
+               --batch_size 4 \
+               --train_crop_size 40 \
                --num_init_epochs 2 \
                --num_epochs 50 \
-               --print_every 600 \
+               --print_every 50 \
                --save_every 10 \
                --val_every 10
+
+#/d/anaconda3/envs/pmypytorch2/python.exe main.py \
+#               --save_dir ./train_self/CUFED/TTSR \
+#               --reset True \
+#               --log_file_name train.log \
+#               --num_gpu 1 \
+#               --num_workers 9 \
+#               --dataset CUFED \
+#               --dataset_dir ./CUFED \
+#               --n_feats 64 \
+#               --lr_rate 1e-4 \
+#               --lr_rate_dis 1e-4 \
+#               --lr_rate_lte 1e-5 \
+#               --rec_w 1 \
+#               --per_w 1e-2 \
+#               --tpl_w 1e-2 \
+#               --adv_w 1e-3 \
+#               --batch_size 2 \
+#               --num_workers 0 \
+#               --num_init_epochs 2 \
+#               --num_epochs 150 \
+#               --print_every 600 \
+#               --save_every 10 \
+#               --val_every 10 \
+#               --model_path ./TTSR.pt    # 使用作者的预训练权重
+
+
 
 
 # ### training TTSR-rec
